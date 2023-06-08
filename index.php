f3456ed1e46cfa270eff564263f7277e6bc12ca9
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-06-08 15:51:47 +0200

    add $arr_character and $password_lenght

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 $arr_character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%^&*()[]';
+$password_lenght = isset($_GET['password']) ?? '';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,6 +14,7 @@ $arr_character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </head>
+
 <body>
     <div class="container mt-5">
         <h1 class="text-center text-primary">Strong Password Generator</h1>
@@ -33,4 +36,5 @@ $arr_character = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
         </form>
     </div>
 </body>
+
 </html>
